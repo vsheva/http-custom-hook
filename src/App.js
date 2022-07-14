e0c1fa1766a820cccf7170ce1cd58fd10e2086f1
vsheva <valera.sheva@gmail.com>
2022-07-14 18:39:08 +0300

    add custom hook to App.js

--- src/App.js
+++ src/App.js
@@ -6,23 +6,22 @@ import useHttp from "./hooks/use-http";
 
 function App() {
     const [tasks, setTasks] = useState([]);
-
-
-    const transformTasks = (tasksObj) => {
-
-        const loadedTasks = [];
-        for (const taskKey in tasksObj) {
-            loadedTasks.push({id: taskKey, text: tasksObj[taskKey].text});
-        }
-        setTasks(loadedTasks);
-    }
-
-    const {isLoading, error, sendRequest: fetchTasks} = useHttp({url: 'https://react-http-53159-default-rtdb.firebaseio.com/tasks.json'}, transformTasks)
-
+    const {isLoading, error, sendRequest: fetchTasks} = useHttp()
 
     useEffect(() => {
-        fetchTasks();
-    }, []);
+        const transformTasks = (tasksObj) => {
+            const loadedTasks = [];
+
+            for (const taskKey in tasksObj) {
+                loadedTasks.push({id: taskKey, text: tasksObj[taskKey].text});
+            }
+            setTasks(loadedTasks);
+        }
+
+        fetchTasks({url: 'https://react-http-53159-default-rtdb.firebaseio.com/tasks.json'}, transformTasks);
+
+    }, [fetchTasks]);
+
 
     const taskAddHandler = (task) => {
         setTasks((prevTasks) => prevTasks.concat(task));
@@ -42,3 +41,8 @@ function App() {
 }
 
 export default App;
+
+
+/*
+
+*/
--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,11 +1,11 @@
-import {useState} from "react";
+import {useCallback, useState} from "react";
 
-const useHttp =(requestConfig, applyData)=> {
+const useHttp =()=> {
 
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
 
-    const sendRequest = async () => {
+    const sendRequest =useCallback(async (requestConfig, applyData) => {
         setIsLoading(true);
         setError(null);
         try {
@@ -27,7 +27,7 @@ const useHttp =(requestConfig, applyData)=> {
             setError(err.message || 'Something went wrong!');
         }
         setIsLoading(false);
-    };
+    },[]);
 
 
 return{
